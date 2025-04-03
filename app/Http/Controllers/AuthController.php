fa463dcb92416d268934bd098d279b475bb37704
HtooMyatDev <itshtunyk@gmail.com>
2025-04-03 12:00:34 +0700

    email & password validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,17 +1,28 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        logger($request->all());
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Login successful',
-        ]);
+        $user = User::where('email', $request->email)->first();
+        if ($user) {
+            // Check if the password is correct
+            if (password_verify($request->password, $user->password)) {
+                // Generate a token for the user
+                return response()->json([
+                    'status' => "success",
+                    'token'  => $user->createToken(time())->plainTextToken,
+                ]);
+            }
+        } else {
+            return response()->json([
+                'status' => 'error',
+            ]);
+        }
         // Validate the request
     }
 }
