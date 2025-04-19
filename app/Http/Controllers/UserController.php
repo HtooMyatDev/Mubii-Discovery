d74330a3813a719a884f8e032be06b482f27c98d
HtooMyatDev <itshtunyk@gmail.com>
2025-04-19 17:15:29 +0700

    profile data through api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -9,5 +8,26 @@ class UserController extends Controller
     public function index()
     {
         return view('user.home');
+    }
+
+    public function update(Request $request)
+    {
+        $data = $this->requestUserData($request);
+        
+        return response()->json([
+            'status' => 'success',
+        ]);
+    }
+
+    private function requestUserData($request)
+    {
+        return [
+            'name'          => $request->name,
+            'profile'       => uniqid() . 'user_profile' . $request->profile,
+            'address'       => $request->address,
+            'email'         => $request->email,
+            'phone_number'  => $request->phone_number,
+            'date_of_birth' => $request->date_of_birth,
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,19 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::group(['prefix'=>'auth'], function(){
+Route::group(['prefix' => 'auth'], function () {
     Route::post('login', [AuthController::class, 'login'])->name('api#login');
-    Route::post('register',[AuthController::class,'register'])->name('api#register');
+    Route::post('register', [AuthController::class, 'register'])->name('api#register');
+});
+
+Route::group(['prefix' => 'profile'], function () {
+    Route::post('update', [UserController::class, 'update'])->name('api#profile#update');
 });
