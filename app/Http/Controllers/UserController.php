bc04eb6ab998ef9b757269c90f28ee169f920bb6
HtooMyatDev <itshtunyk@gmail.com>
2025-04-20 21:23:25 +0700

    subtle backend changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -14,38 +13,42 @@ class UserController extends Controller
 
     public function update(Request $request)
     {
-        $validators = $this->checkValidation($request);
-        if ($validators->fails()) {
-            logger('tf?');
-            return response()->json([
-                'status' => false,
-                'errors' => $validators->errors(),
-            ],422);
-        } else {
-            $data       = $this->requestUserData($request);
-            $oldProfile = User::select('profile')->where('id', $request->id)->first();
+        // $validators = $this->checkValidation($request);
+        // if ($validators->fails()) {
+        //     logger('tf?');
+        //     return response()->json([
+        //         'status' => false,
+        //         'errors' => $validators->errors(),
+        //     ],422);
+        // } else {
+        //     $data       = $this->requestUserData($request);
+        //     $oldProfile = User::select('profile')->where('id', $request->id)->first();
 
-            if ($request->hasFile('profile')) {
-                if ($oldProfile->profile != null) {
-                    if (file_exists(public_path('user/profile/' . $oldProfile->profile))) {
-                        unlink(public_path('user/profile/' . $oldProfile->profile));
-                    }
-                }
-                $fileName = uniqid() . $request->file('profile')->getClientOriginalName();
-                $request->file('profile')->move(public_path() . '/user/profile/', $fileName);
-                $data['profile'] = $fileName;
-            } else {
-                $data['profile'] = $oldProfile->profile;
-            }
+        //     if ($request->hasFile('profile')) {
+        //         if ($oldProfile->profile != null) {
+        //             if (file_exists(public_path('user/profile/' . $oldProfile->profile))) {
+        //                 unlink(public_path('user/profile/' . $oldProfile->profile));
+        //             }
+        //         }
+        //         $fileName = uniqid() . $request->file('profile')->getClientOriginalName();
+        //         $request->file('profile')->move(public_path() . '/user/profile/', $fileName);
+        //         $data['profile'] = $fileName;
+        //     } else {
+        //         $data['profile'] = $oldProfile->profile;
+        //     }
 
-            User::where('id', $request->id)->update($data);
-            $updatedData = User::find($request->id);
+        //     User::where('id', $request->id)->update($data);
+        //     $updatedData = User::find($request->id);
 
-            return response()->json([
-                'status'      => true,
-                'updatedData' => $updatedData,
-            ],200);
-        }}
+        //     return response()->json([
+        //         'status'      => true,
+        //         'updatedData' => $updatedData,
+        //     ],200);
+        return response()->json([
+            'status'  => true,
+            'message' => 'fantastic',
+        ], 200);
+    }
 
     private function requestUserData($request)
     {
@@ -61,8 +64,8 @@ class UserController extends Controller
     private function checkValidation($request)
     {
         $validationRules = [
-            'name'    => 'required',
-            'email'   => 'required|unique:users,email,' . $request->id,
+            'name'  => 'required',
+            'email' => 'required|unique:users,email,' . $request->id,
         ];
 
         return Validator::make($request->all(), $validationRules);
